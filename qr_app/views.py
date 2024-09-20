e96b28743bb11b75f41a5260da1b76fb94e3c619
Kizito Daka <kizitodak@gmail.com>
2024-09-20 02:08:31 +0100

    Update views.py

--- qr_app/views.py
+++ qr_app/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 from django.db.models import Q
