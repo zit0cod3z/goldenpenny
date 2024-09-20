84347f949959bfc724d7bc7615ac94233e858698
Kizito Daka <kizitodak@gmail.com>
2024-09-20 02:21:42 +0100

    Update serializers.py

--- qr_app/serializers.py
+++ qr_app/serializers.py
@@ -1,7 +1,7 @@
 # from rest_framework import serializers
-from .models import User
+# from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['name', 'email', 'phone_number']
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['name', 'email', 'phone_number']
